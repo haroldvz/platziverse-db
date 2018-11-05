3f58e8ed7539b02b3ee0089d818752b2985fa9b9
haroldvz <velezh010@gmail.com>
2018-11-05 13:40:33 -0500

    Adding prompt question for setup dabatase and colors for error messages

--- setup.js
+++ setup.js
@@ -1,9 +1,25 @@
 'use strict'
 
 const debug = require('debug')('platziverse:db:setup')
-const db = require('./')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const db = require('./')// require the index.js
+
+const prompt = inquirer.createPromptModule()// Obj que permite hacer preguntas en la consola y tomar decisiones con ellas (son promesas, hay que resolver promesas)
 
 async function setup () {
+
+  // Esperar por prompt
+  const asnwer = await prompt([{
+    type: 'confirm',//pregunta si o no,
+    name: 'setup',// la pregunta la guarda en propiedad llamada setup
+    message: 'This will destroy your bd, are you sure?'
+  }])
+
+  if (!asnwer.setup){
+    return console.log('Nothing happened ...')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
@@ -21,8 +37,9 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.mesage)
-  console.error(err.mesage)
+  console.log('HANDLER')
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+  console.error(`${chalk.blue('[error stack]')} ${err.stack}`)
   process.exit(1)
 }
 
